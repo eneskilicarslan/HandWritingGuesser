2ca51cf9f17a9a8e0c14915efb2fb6e47023150c
Enes Kılıçarslan <37044465+eneskilicarslan@users.noreply.github.com>
2021-03-12 12:21:50 +0300

    GUI application file upload
    used packages: Numsharp

---
+++ HandWritingGuesser/MainWindow.xaml.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using NumSharp;
+
+namespace HandWritingGuesser
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        DrawingAttributes inkDA;
+        public MainWindow()
+        {
+            InitializeComponent();
+            inkDA = new DrawingAttributes();
+            inkDA.Height = 7;
+            inkDA.Width = 7;
+            inkDA.FitToCurve = false;
+
+            // Set up the DrawingAttributes for the highlighter.
+            
+            inkCanvas.DefaultDrawingAttributes = inkDA;
+        }
+
+        public static Bitmap ResizeImage(System.Drawing.Image image, int width, int height)
+        {
+            var destRect = new System.Drawing.Rectangle(0, 0, width, height);
+            var destImage = new Bitmap(width, height);
+
+            destImage.SetResolution(image.HorizontalResolution, image.VerticalResolution);
+
+            using (var graphics = Graphics.FromImage(destImage))
+            {
+                graphics.CompositingMode = CompositingMode.SourceCopy;
+                graphics.CompositingQuality = CompositingQuality.HighQuality;
+                graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                graphics.SmoothingMode = SmoothingMode.HighQuality;
+                graphics.PixelOffsetMode = PixelOffsetMode.HighQuality;
+
+                using (var wrapMode = new ImageAttributes())
+                {
+                    wrapMode.SetWrapMode(WrapMode.TileFlipXY);
+                    graphics.DrawImage(image, destRect, 0, 0, image.Width, image.Height, GraphicsUnit.Pixel, wrapMode);
+                }
+            }
+
+            return destImage;
+        }
+
+        public byte[] ImageToByteArray(System.Drawing.Image imageIn)
+        {
+            using (var ms = new MemoryStream())
+            {
+                imageIn.Save(ms, System.Drawing.Imaging.ImageFormat.Jpeg);
+                return ms.ToArray();
+            }
+        }
+        public static byte[] ImageToByte(System.Drawing.Image img)
+        {
+            ImageConverter converter = new ImageConverter();
+            return (byte[])converter.ConvertTo(img, typeof(byte[]));
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            RenderTargetBitmap rtb = new RenderTargetBitmap((int)inkCanvas.Width, (int)inkCanvas.Height, 96d, 96d, PixelFormats.Default);
+            rtb.Render(inkCanvas);
+            BmpBitmapEncoder encoder = new BmpBitmapEncoder();
+            encoder.Frames.Add(BitmapFrame.Create(rtb));
+            FileStream fs = File.Open(@"C:\Users\Enes\Desktop\test\test.jpg", FileMode.Create);
+            encoder.Save(fs);
+            fs.Close();
+
+            this.inkCanvas.Strokes.Clear();
+
+            System.Drawing.Image img = System.Drawing.Image.FromFile(@"C:\Users\Enes\Desktop\test\test.jpg");
+            img = ResizeImage(img, 28, 28);
+
+            byte[] byteArr = ImageToByte(img);
+
+            Console.WriteLine(byteArr.Length);
+        }
+    }
+}
